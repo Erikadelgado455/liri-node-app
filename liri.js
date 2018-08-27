0a19c38f657f117a0db88607cc8ca9e251f4d5c1
erikadelgado455 <erikadelgado455@yahoo.com>
2018-08-27 06:09:00 -0400

    added a function that displays a song with the Artist, song name, preview link, and Album

--- liri.js
+++ liri.js
@@ -10,3 +10,20 @@ function twitter(inputs) {
 			}
 		});
 }
+function spotify(inputs) {
+	var spotify = new Spotify(keys.spotifyKeys);
+		if (!inputs){
+        	inputs = 'The Sign';
+    	}
+		spotify.search({ type: 'track', query: inputs }, function(err, data) {
+			if (err){
+	            console.log('Error occurred: ' + err);
+	            return;
+	        }
+	        var songInfo = data.tracks.items;
+	        console.log("Artist(s): " + songInfo[0].artists[0].name);
+	        console.log("Song Name: " + songInfo[0].name);
+	        console.log("Preview Link: " + songInfo[0].preview_url);
+	        console.log("Album: " + songInfo[0].album.name);
+	});
+}
